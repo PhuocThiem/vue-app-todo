527a392bbe3cdf1ffe81a52114dac68dafd07cc7
Minh Le Ngoc <minhle@Minhs-MacBook-Pro.local>
2019-11-02 10:52:19 +0700

    test fix lint

--- src/service/http.js
+++ src/service/http.js
@@ -8,32 +8,32 @@ axios.defaults.timeout = 30000;
 axios.interceptors.request.use(config => config, error => Promise.reject(error));
 
 axios.interceptors.response.use(
-  (response) => {
+  response => {
     console.log('inside interceptors', response);
     return response;
   },
-  error => Promise.reject(error.response || error.request || error.message)
+  error => Promise.reject(error.response || error.request || error.message),
 );
 
 const http = {
-  request (config = {}) {
+  request(config = {}) {
     return axios.request(config);
   },
-  get (url, config = {}) {
+  get(url, config = {}) {
     return axios.get(url, config);
   },
-  post (url, data = {}, config = {}) {
+  post(url, data = {}, config = {}) {
     return axios.post(url, data, config);
   },
-  put (url, data = {}, config = {}) {
+  put(url, data = {}, config = {}) {
     return axios.put(url, data, config);
   },
-  patch (url, data = {}, config = {}) {
+  patch(url, data = {}, config = {}) {
     return axios.patch(url, data, config);
   },
-  delete (url, config = {}) {
+  delete(url, config = {}) {
     return axios.delete(url, config);
-  }
+  },
 };
 
 export default http;
